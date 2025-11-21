3ee8e615bc9839a0d0fa70ef57fb702de3ecd732
LayrCrismer <fedotov112@list.ru>
2025-11-21 17:21:14 +0500

    Добавлены функции поиска позиции пустой ячейки и перемещения плиток
    //Пестринин Даниил

--- functions.cpp
+++ functions.cpp
@@ -31,3 +31,52 @@ void draw_board(const int board[SIZE][SIZE]) { //отрисовка игрово
         cout<<endl;
     }
 }
+
+void find_emptyslot(const int board[SIZE][SIZE], int& empty_row, int& empty_col) { //поиск позиции пустой ячейки (обозначена нулем)
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (board[i][j] == 0) {
+                empty_row = i;
+                empty_col = j;
+                return; //выход при первой найденой пустой ячейке
+            }
+        }
+    }
+}
+
+void swapSlots(int& a, int& b) { //вспомоготальеная перемещения плиток  - обмен значений двух переменных
+    int temp = a;
+    a=b;
+    b=temp;
+}
+
+bool moveTile(int board[SIZE][SIZE],char move) { //перемещение плитки относительно пустой ячейки
+    int e_UD, e_RL; //координаты пустой ячейки UD - Up/Down, RL - Right/Left
+    find_emptyslot(board, e_UD, e_RL);
+
+    int t_UD = e_UD; //координаты ячейки для перемещения
+    int t_RL = e_RL;
+
+    //определение координат ячейки которую нужно переместить
+    switch (move) {
+        case 'w':
+            t_UD = e_UD+1;
+            break;
+        case 's':
+            t_UD = e_UD-1;
+            break;
+        case 'a':
+            t_RL = e_RL+1;
+            break;
+        case 'd':
+            t_RL = e_RL-1;
+            break;
+
+    }
+    //проверка правильности хода
+    if (t_UD >= 0 && t_UD < SIZE && t_RL >= 0 && t_RL < SIZE) {
+        swapSlots(board[e_UD][e_RL], board[t_UD][t_RL]); //обмен пустой и целевой ячейки
+        return true;//успех
+    }
+    return false;//недопустимый ход
+}
