311332ac6e4e5431e01fa89473bace3a00359168
LayrCrismer <fedotov112@list.ru>
2025-11-21 17:18:07 +0500

    Добавлена функция генерации игрового поля
    //Пестринин Даниил

--- functions.cpp
+++ functions.cpp
@@ -2,5 +2,16 @@
 // Created by fedot on 21.11.2025.
 //
 #include <iostream>
+#include <iomanip>
 #include "head.h"
 using namespace std;
+
+void board_generation(int board[SIZE][SIZE]) { //генерация игрового поля, где каждая клетка - упорядоченное число от 1 до SIZE*SIZE-1
+    int count = 1;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            board[i][j] = count++;
+        }
+    }
+    board[SIZE-1][SIZE-1] = 0; //правый нижний угол - пустой
+}
