4c39cb1c9ed26a08712be3f2eae5ccc29fbfd1f5
LayrCrismer <fedotov112@list.ru>
2025-11-21 17:17:34 +0500

    Введены 2 главные глобальные переменные
    //Пестринин Даниил

--- heads.h
+++ heads.h
@@ -5,6 +5,8 @@
 #ifndef LAB_NUM6_HEADS_H
 #define LAB_NUM6_HEADS_H
 
+const int SIZE = 4; //размер доски
+const int empty_tile = SIZE*SIZE; //последний слот делается пустым
 
 
 #endif //LAB_NUM6_HEADS_H
