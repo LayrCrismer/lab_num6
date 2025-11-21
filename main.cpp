ce8184f616468957e8eadb3b5f08809606bed5a4
LayrCrismer <fedotov112@list.ru>
2025-11-21 17:22:16 +0500

    Функции в main.cpp инициализированы
    //Пестринин Даниил

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@ using namespace std;
 
 void board_generation(int board[SIZE][SIZE]);
 void draw_board(const int board[SIZE][SIZE]);
+void find_emptyslot(const int board[SIZE][SIZE],int& empty_row, int& empty_col);
+void swapSlots(int& a, int& b);
+bool moveTile(int board[SIZE][SIZE], char move);
+void mixTiles(int board[SIZE][SIZE]);
+
 
 int main() {
     int board[SIZE][SIZE];
